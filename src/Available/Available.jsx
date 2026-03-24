c60a0314347029d909179cd633849926bc8b35fb
MaksumulEmon <maksumulemon300@gmail.com>
2026-03-25 00:43:02 +0600

    Card Sectiob & responsive

--- src/Available/Available.jsx
+++ src/Available/Available.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFlag, FaUser } from 'react-icons/fa';
+import Card from '../Ui/Card';
 
 const Available = ({ players }) => {
     console.log(players, "players");
@@ -11,55 +11,13 @@ const Available = ({ players }) => {
             {
                 players.map(player => {
                     console.log(player);
-                    return <div className='container mx-auto'>
-                        <div className="card bg-base-100  shadow-sm p-4">
-                            <figure>
-                                <img className='rounded'
-                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                    alt="Shoes" />
-                            </figure>
-                            {/* ------------------------------------------------ */}
-                            <div className="flex items-center pt-1 gap-2">
-                                <FaUser></FaUser>
-                                <h1 className='text-xl font-semibold'>{player.playerName}</h1>
-                            </div>
-
-                            {/* --------------------------------------------------- */}
-                            <div className='flex justify-between items-center'>
-                                <div className='flex gap-3 items-center'>
-                                    <FaFlag></FaFlag>
-                                    <p>India</p>
-                                </div>
-
-                                <button className=" pt-2 pb-2 pl-4 pr-4 bg-[#1313130D] rounded">All-Rounder</button>
-                            </div>
-
-                            <hr className='border border-[#1313131A] mt-4' />
-
-
-                            <div className='space-y-3'>
-                                <h1 className='font-bold'>Rating</h1>
-
-                                <div className='flex justify-between'>
-                                    <p className='font-semibold text-[#131313]'>Left-Hand-Bat</p>
-                                    <p className='text-[#131313B3]'>Left-Hand-Bat</p>
-                                </div>
-
-                                {/* ---------------------------------------- */}
-
-                                <div className='flex justify-between items-center'>
-                                    <p className='font-semibold text-[#131313]'>Price: $1500000</p>
-                                    <button className='text-[#131313B3] p-1 pl-2 pr-2 rounded border-4 border-[#E7FE29]
-                        active:bg-gray-700 active:scale-95 transition-all duration-150'>Choose Player</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                })
-            }
-
-
+                    return (
+                    <Card player={player}></Card>
+                    )
+                })}
         </div>
+
+
     );
 };
 
---
+++ src/Ui/Card.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { FaFlag, FaUser } from 'react-icons/fa';
+
+
+const Card = ({ player }) => {
+    return (
+
+
+
+
+        <div className="card bg-base-100  shadow-sm p-4">
+            <figure>
+                <img className='rounded'
+                    src={player.playerImg}
+                    alt="Shoes" />
+            </figure>
+            {/* ------------------------------------------------ */}
+            <div className="flex items-center pt-1 gap-2">
+                <FaUser></FaUser>
+                <h1 className='text-xl font-semibold'>{player.playerName}</h1>
+            </div>
+
+            {/* --------------------------------------------------- */}
+            <div className='flex justify-between items-center'>
+                <div className='flex gap-3 items-center'>
+                    <FaFlag></FaFlag>
+                    <p>{player.playerCountry}</p>
+                </div>
+
+                <button className=" pt-2 pb-2 pl-4 pr-4 bg-[#1313130D] rounded">All-Rounder</button>
+            </div>
+
+            <hr className='border border-[#1313131A] mt-4' />
+
+
+            <div className='space-y-3'>
+                <h1 className='font-bold pt-2'>Rating: {player.rating}</h1>
+
+                <div className='flex justify-between'>
+                    <p className='font-semibold text-[#131313]'>{player.battingStyle}</p>
+                    <p className='text-[#131313B3]'>{player.bowlingStyle}</p>
+                </div>
+
+                {/* ---------------------------------------- */}
+
+                <div className='flex justify-between items-center'>
+                    <p className='font-semibold text-[#131313]'>{player.price}</p>
+                    <button className='text-[#131313B3] p-1 pl-2 pr-2 rounded border-4 border-[#E7FE29]
+                                active:bg-gray-700 active:scale-95 transition-all duration-150'>Choose Player</button>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+    );
+};
+
+export default Card;
